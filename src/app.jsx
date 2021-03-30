f452ca9a3702555794e042015f8235bb63477392
changhyuk lee <cantabile26@naver.com>
2021-03-30 17:32:01 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
+
 
 function App() {
-  return (
-    <h1>Hello :)!!</h1> 
-  );
+  const [videos ,setVideos] = useState([]);  
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAl8XvuaC9VY28koolTip-LeREswKwZ3Cw"
+      , requestOptions
+      )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+      
+  }, []);
+  
+  return <VideoList videos={videos} />;
+  
 }
 
 export default App;
