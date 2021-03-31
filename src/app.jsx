a9b80f5d2526d1d155d72999b38115fc0e45e4bb
changhyuk lee <cantabile26@naver.com>
2021-03-31 16:06:38 +0900

    make youtube search header

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 
-function App() {
+function App({youtube}) {
   const [videos ,setVideos] = useState([]);  
+  const search = query => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
+  };
+
+  const main = () => {
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAl8XvuaC9VY28koolTip-LeREswKwZ3Cw"
-      , requestOptions
-      )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-      
+    main();      
   }, []);
   
-  return <VideoList videos={videos} />;  
+  return (
+    <div className={styles.app}>
+      <SearchHeader 
+        onSearch={search}
+        onMain={main}
+      />
+      <VideoList videos={videos} />    
+    </div>
+  );  
   
 }
 
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+const SearchHeader = ({onSearch, onMain}) => {
+        const inputRef = useRef();   
+        const handleMain = () => {              
+            const clear = "";
+            inputRef.current.value = clear;
+            onMain();
+        }     
+        const handleSearch = () => {
+            const value = inputRef.current.value;
+            
+            onSearch(value);
+        };
+        const onClick = () => {
+            handleSearch();
+        };
+
+        const onKeyPress = event => {
+            if(event.key === 'Enter'){
+                handleSearch();
+            }            
+        };
+        return(
+            <header className={styles.header}>
+                <div className={styles.logo} onClick={handleMain}>
+                    <img className={styles.img} src="/images/logo.png" alt="logo"/>
+                    <h1 className={styles.title}>Youtube</h1>
+                </div>
+                <input ref={inputRef} className={styles.input} type="search" name="" id="" placeholder="Search" onKeyPress={onKeyPress}/>
+                <button className={styles.button} type="submit" onClick={onClick}>
+                    <img className={styles.buttonImg}src="/images/search.png" alt="search"/>
+                </button>
+            </header>
+        )            
+    };
+
+export default SearchHeader;
